aa374d62d19f0e0f539471927962a26b05c3bdb6
Mario Falomir <mariofalomir@gmail.com>
2012-08-24 17:16:27 -0700

    Support for multiple lookup methods

--- src/FullContact.php
+++ src/FullContact.php
@@ -6,57 +6,58 @@ define ("BASE_URL", "https://api.fullcontact.com/");
 define ("API_VERSION", "v2");
 
 class FullContactAPI {
-    
+
     private $_apiKey = null;
-    
+
     /*
-     *
-     * @param String $token_id
-     * 
-     */
+    *
+    * @param String $token_id
+    *
+    */
     public function __construct($api_key) {
         $this->_apiKey = $api_key;
     }
-    
+
     /*
-     * Return an array of data about a specific email address
-     *
-     * @param String - Email address
-     * @param String (optional) - timeout
-     *
-     * @return Array - All information associated with this email address
-     */
-    public function doLookup($email = null, $timeout = 30) {
+    * Return an array of data about a specific email address/phone number -- Mario Falomir http://github.com/mariofalomir
+    *
+    * @param String - Search Term (Could be an email address or a phone number, depending on the specified search type)
+    * @param String - Search Type (Specify the API search method to use. E.g. email -- tested with email and phone)
+    * @param String (optional) - timeout
+    *
+    * @return Array - All information associated with this email address
+    */
+    public function doLookup($term = null, $type="email", $timeout = 30) {
         $return_value = null;
-        
-        if ($email != null) {
-            
-            $result = $this->restHelper(BASE_URL . API_VERSION . "/person.json?email=" . urlencode($email) . "&apiKey=" . urlencode($this->_apiKey) . "&timeoutSeconds=" . urlencode($timeout));
-            
+
+        if ($term != null) {
+
+            $result = $this->restHelper(BASE_URL . API_VERSION . "/person.json?{$type}=" . urlencode($term) . "&apiKey=" . urlencode($this->_apiKey) . "&timeoutSeconds=" . urlencode($timeout));
+
             if ($result != null) {
                 $return_value = $result;
             }//end inner if
         }//end outer if
-        
+
         return $return_value;
     }
-    
-/****************************************************************************/     
-/****************************************************************************/     
-     
+
+    /****************************************************************************/
+    /****************************************************************************/
+
     /*********************************
      **** PRIVATE helper function ****
      *********************************/
     function restHelper($json_endpoint) {
-        
+
         $return_value = null;
-        
+
         $http_params = array(
             'http' => array(
                 'method' => "GET",
-                'ignore_errors' => true 
-        ));
-        
+                'ignore_errors' => true
+            ));
+
         $stream_context = stream_context_create($http_params);
         $file_pointer = fopen($json_endpoint, 'rb', false, $stream_context);
         if (!$file_pointer) {
@@ -67,18 +68,18 @@ class FullContactAPI {
             $stream_contents = stream_get_contents($file_pointer);
         }
         if ($stream_contents !== false) {
-            
+
             if (strlen($stream_contents) > 0) {
-                
+
                 //We're receiving stream data back from the API, json decode it here.                
                 $result = json_decode($stream_contents, true);
-                
+
                 //if result is NULL we have some sort of error
                 if ($result === null) {
-                    
+
                     $return_value = array();
                     $return_value['is_error'] = true;
-                    
+
                     //does the stream meta data give us something to go on?
                     if (isset($stream_meta_data['wrapper_data'][0])) {
 
@@ -93,24 +94,24 @@ class FullContactAPI {
                          */
                         if (strpos($stream_meta_data['wrapper_data'][0], "403") !== false) {
                             $return_value['error_message'] = "Your API key is invalid, missing, or has exceeded its quota.";
-                            
+
                         } else if (strpos($stream_meta_data['wrapper_data'][0], "422") !== false) {
                             $return_value['error_message'] = "The server understood the content type and syntax of the request but was unable to process the contained instructions (Invalid email).";
                         }
                     }//end if (isset)
-                    
+
                 } else {
 
-                    $result['is_error'] = false;                    
+                    $result['is_error'] = false;
                     $return_value = $result;
                 }
             }//end if (strlen)
-            
-        //The stream_contents failed.
+
+            //The stream_contents failed.
         } else {
             throw new Exception("$verb $json_endpoint failed");
         }//end outer else
-        
+
         return $return_value;
     }//end restHelper
 }//end FullContactAPI
