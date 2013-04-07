85a38afe9e17a127caf60a59752752637877888b
D Keith Casey Jr <keith@caseysoftware.com>
2013-04-06 20:46:14 -0500

    sketching out the autoloader and base class; this extends the existing FC class to keep backwards compatibility;

---
+++ Services/FullContact.php
@@ -0,0 +1,28 @@
+<?php
+
+function Services_FullContact_autoload($className) {
+    $library_name = 'Services_FullContact';
+    
+    if (substr($className, 0, strlen($library_name)) != $library_name) {
+        return false;
+    }
+    $file = str_replace('_', '/', $className);
+    $file = str_replace('Services/', '', $file);
+    return include dirname(__FILE__) . "/$file.php";
+}
+
+spl_autoload_register('Services_FullContact_autoload');
+
+include_once '../src/FullContact.php';
+
+/**
+ * This class doesn't do much yet..
+ *
+ * @package  Services\FullContact
+ * @author   Keith Casey <contrib@caseysoftware.com>
+ * @license  http://creativecommons.org/licenses/MIT/ MIT
+ */
+class Services_FullContact extends FullContactAPI
+{
+    
+}
